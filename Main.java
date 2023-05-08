35ab677c192ffa73316dc803a2a59830dc79a99f
pshsh910 <pshsh910@gmail.com>
2023-05-08 14:14:22 +0900

    fix: 장난꾸러기 문제 추가

--- Main.java
+++ Main.java
@@ -1,25 +1,11 @@
 /*
-[[ 장난꾸러기 ]]
-120 125 152 130 135 135 143 127 160
+[[  ]]
 
-정렬된 배열과 비교
-
-얕은 복사와 깊은 복사를 이해해야 함
  */
 import java.util.*;
 
 public class Main {
     public ArrayList<Integer> solution(int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] compareArr = arr.clone();
-
-        Arrays.sort(compareArr);
-
-        for(int i = 0; i < arr.length; i++) {
-            if(compareArr[i] != arr[i]) {
-                answer.add(i+1);
-            }
-        }
 
         return answer;
     }
