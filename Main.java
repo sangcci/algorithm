ec1e37ecda27625dfc9d47867100fe6f798df845
pshsh910 <pshsh910@gmail.com>
2023-05-10 16:02:59 +0900

    fix: 좌표 정렬 문제 추가

--- Main.java
+++ Main.java
@@ -5,23 +5,25 @@
 import java.util.*;
 
 public class Main {
-    public ArrayList<Integer> solution(int[] arr) {
 
-        return answer;
+    public int[][] solution(int[][] arr) {
+
+        return arr;
     }
 
     public static void main(String[] args) {
-        Main m = new Main();
+        //Main m = new Main();
         Scanner in = new Scanner(System.in);
 
         int N = in.nextInt();
-        int[] arr = new int[N];
+        int[][] arr = new int[N][2];
         for(int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
         }
 
-        for(Integer e : m.solution(arr)) {
-            System.out.print(e + " ");
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
 }
