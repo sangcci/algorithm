55164832966c1a29bea2676547833d948e0bffd0
baksakcci <pshsh910@gmail.com>
2023-05-13 00:44:30 +0900

    feat: 삼각형의 완성 조건 추가

--- Main.java
+++ Main.java
@@ -6,22 +6,18 @@ import java.util.*;
 
 public class Main {
 
-    public int solution(int M, int[] arr) {
-        return -1;
+    public int solution(int n1, int n2) {
+
+        return count;
     }
 
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
 
-        int N = in.nextInt();
-        int M = in.nextInt();
+        int n1 = in.nextInt();
+        int n2 = in.nextInt();
 
-        int[] arr = new int[N];
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
-        }
-
-        System.out.print(m.solution(M, arr));
+        System.out.print(m.solution(n1, n2));
     }
 }
