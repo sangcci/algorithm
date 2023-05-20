cb73eaa4f2532874fbd6f2a2d4d4c001c89ae58e
baksakcci <pshsh910@gmail.com>
2023-05-20 19:03:40 +0900

    feat: 마구간 구하기 (결정 알고리즘) 추가

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import java.util.*;
+/*
+[[  ]]
 
+ */
 public class Main {
 
-    public int solution(int[][] board) {
+    public int solution(int count) {
         int answer = 0;
 
         return answer;
     }
+
 
     public static void main(String[] args) {
         Main m = new Main();
@@ -14,12 +18,6 @@ public class Main {
 
         int N = in.nextInt();
 
-        int[][] board = new int[N][N];
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                board[i][j] = in.nextInt();
-            }
-        }
-        System.out.print(m.solution(board));
+        System.out.print(m.solution(N));
     }
 }
