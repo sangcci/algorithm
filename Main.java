e6c47f0603c31148cf7f004bdc471798a804ee56
baksakcci <pshsh910@gmail.com>
2023-06-25 18:21:32 +0900

    feat: 트리 말단 노드까지의 가장 짧은 경로(BFS) 추가

--- Main.java
+++ Main.java
@@ -4,10 +4,10 @@ import java.util.*;
 
  */
 public class Main {
-    public int BFS(int S, int E) {
+
+    public void BFS() {
 
     }
-
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
