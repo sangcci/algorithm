2bd4591dff0b8eaeec61399af88a0ba747e06f5b
baksakcci <pshsh910@gmail.com>
2023-06-29 15:52:58 +0900

    feat: 합이 같은 부분집합 (DFS) 추가

--- Main.java
+++ Main.java
@@ -4,14 +4,12 @@ import java.util.*;
 
  */
 public class Main {
-
-    public void BFS(int v) {
+    public void DFS(int l, int sum) {
 
     }
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
-
 
     }
 }
---
+++ dfs/SubSet2.java
@@ -0,0 +1,40 @@
+package dfs;
+import java.util.*;
+/*
+[[ 합이 같은 부분집합 (DFS : 아마존 인터뷰) ]]
+
+ */
+public class SubSet2 {
+    public static int N;
+    public static int setSum;
+    public static int[] set;
+    public static String answer;
+    public void DFS(int l, int sum) {
+        if(answer.equals("YES") || sum > (setSum-sum)) return;
+        if(setSum - sum == sum) {
+            answer = "YES";
+        }
+        else {
+            if(l < N) {
+                DFS(l+1, sum + set[l]);
+                DFS(l+1, sum);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        SubSet2 m = new SubSet2();
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        set = new int[N];
+        for(int i = 0; i < N; i++) {
+            set[i] = in.nextInt();
+            setSum += set[i];
+        }
+        answer = "NO";
+
+        m.DFS(0, 0);
+
+        System.out.println(answer);
+    }
+}
