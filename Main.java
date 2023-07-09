e0d399c82624e95d8dd53535aa4f2d09653eeb14
baksakcci <pshsh910@gmail.com>
2023-07-09 22:43:30 +0900

    feat: 바둑이 승차 dfs 문제 추가

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ import java.util.*;
 
  */
 public class Main {
-    public void solution(int n, int k) {
+    public void dfs(int l, int sum) {
 
     }
 
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
+
 
     }
 }
---
+++ dfs/RidingBaduk.java
@@ -0,0 +1,38 @@
+package dfs;
+/*
+[[ 바둑이 승차 ]]
+
+ */
+import java.util.Scanner;
+
+public class RidingBaduk {
+    static int C, N;
+    static int[] wArr;
+    static int answer = 0;
+    public void dfs(int l, int sum) {
+        if(sum > C) return;
+        if(l == N) {
+            if(sum < C && sum > answer) {
+                answer = sum;
+            }
+        } else {
+            dfs(l+1, sum + wArr[l]);
+            dfs(l+1, sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        RidingBaduk m = new RidingBaduk();
+        Scanner in = new Scanner(System.in);
+
+        C = in.nextInt();
+        N = in.nextInt();
+        wArr = new int[N];
+        for(int i = 0; i < wArr.length; i++) {
+            wArr[i] = in.nextInt();
+        }
+
+        m.dfs(0, 0);
+        System.out.print(answer);
+    }
+}
