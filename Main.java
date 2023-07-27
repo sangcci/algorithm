81d5cd69220c92dcb3cda89246d537c3347ac131
baksakcci <pshsh910@gmail.com>
2023-07-27 15:20:57 +0900

    feat: 동전 교환 BFS(or DFS) 추가

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ import java.util.*;
 
  */
 public class Main {
-    public void dfs() {
+    public int bfs() {
 
+        return 0;
     }
-
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
