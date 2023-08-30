67bde152ecab10570597014da6bf17b8677e73b4
baksakcci <pshsh910@gmail.com>
2023-08-31 06:57:17 +0900

    feat: 씨름 선수 - Java 알고리즘 강의 그리디 문제 추가

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
-import java.util.*;
-
 /*
-[[  ]]
+[[ ]]
 
  */
+import java.util.*;
 public class Main {
-    public int bfs() {
-
-        return 0;
-    }
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
+
     }
 }
