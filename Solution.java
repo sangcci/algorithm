e393c7b9697bbe9aefd476c699c8ce4f2339a557
baksakcci <pshsh910@gmail.com>
2023-12-27 16:29:01 +0900

    feat: 알고리즘 문제들 업로드

---
+++ Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+//    public static void main(String args[]) throws Exception {
+//        Scanner in = new Scanner(System.in);
+//        int T = in.nextInt();
+//
+//        for (int testcase = 1; testcase <= T; testcase++) {
+//            int n = in.nextInt();
+//            int k = in.nextInt();
+//            int result = 1;
+//            int count = 1;
+//
+//            int[] power = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                power[i] = in.nextInt();
+//            }
+//
+//            Arrays.sort(power);
+//
+//            for (int i = 0; i < n; i++) {
+//                int cur = power[i];
+//                for (int j = i+1; j < n; j++) {
+//                    int compare = power[j] - cur;
+//                    if (compare <= k) {
+//                        count++;
+//                    }
+//                }
+//
+//                result = Math.max(result, count);
+//                count = 1;
+//            }
+//
+//            System.out.println("#" + testcase + " " + result);
+//        }
+//    }
+}
---
+++ String/FindChar.java
@@ -0,0 +1,31 @@
+package String;
+
+import java.util.Scanner;
+
+public class FindChar {
+
+    public int countSameWord(String str, String exChar) {
+        str = str.toLowerCase();
+        exChar = exChar.toLowerCase();
+        int result = 0;
+
+        for (char each : str.toCharArray()) {
+            if (each == exChar.charAt(0)) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        FindChar findChar = new FindChar();
+
+        String str = in.next();
+        String exChar = in.next();
+
+        int answer = findChar.countSameWord(str, exChar);
+        System.out.println(answer);
+    }
+}
