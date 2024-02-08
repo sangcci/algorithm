978b4e6acca820c48182793d2cedc62810553d35
baksakcci <pshsh910@gmail.com>
2024-02-08 18:45:42 +0900

    feat(\dynamic): 인프런 계단 오르기, 인프런 돌다리 건너기 문제 추가

---
+++ dynamic/인프런돌다리건너기.java
@@ -0,0 +1,22 @@
+package dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 인프런돌다리건너기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] answers = new int[n+1];
+        answers[0] = 1;
+        answers[1] = 2;
+        for (int i = 2; i <= n; i++) {
+            answers[i] = answers[i - 2] + answers[i - 1];
+        }
+
+        System.out.println(answers[n]);
+    }
+}
