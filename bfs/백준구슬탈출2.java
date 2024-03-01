96e7339a3444c7cac85fed49dee24469acd0c822
baksakcci <pshsh910@gmail.com>
2024-03-01 11:38:08 +0900

    백준 구슬 탈출 2 문제 패키지 경로 수정 - dfs -> bfs

--- bfs/백준구슬탈출2.java
+++ bfs/백준구슬탈출2.java
@@ -1,9 +1,8 @@
-package dfs;
+package bfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
